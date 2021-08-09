19babfc7a60d1159710cffeb228a9674eae76158
Tolunay <tasdemirtolunay@gmail.com>
2021-08-10 01:11:23 +0300

    Course and Instructor model files updated.

--- school-management/src/dev/patika/models/Course.java
+++ school-management/src/dev/patika/models/Course.java
@@ -3,6 +3,7 @@ package dev.patika.models;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class Course {
@@ -20,5 +21,75 @@ public class Course {
     @ManyToMany
     private List<Student> students =new ArrayList<>();
 
+    public Course(String courseName, String courseCode, double creditScore) {
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+        this.creditScore = creditScore;
+    }
+
+    public Course() {
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public double getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(double creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseName='" + courseName + '\'' +
+                ", courseCode='" + courseCode + '\'' +
+                ", creditScore=" + creditScore +
+                '}';
+    }
 
 }
--- school-management/src/dev/patika/models/Instructor.java
+++ school-management/src/dev/patika/models/Instructor.java
@@ -3,6 +3,7 @@ package dev.patika.models;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class Instructor {
@@ -11,6 +12,74 @@ public class Instructor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    private String instructorName;
+    private String instructorAdress;
+    private String phoneNumber;
+
     @OneToMany(mappedBy = "instructor")
     private List<Course> courseList = new ArrayList<>();
+
+    public Instructor(String instructorName, String instructorAdress, String phoneNumber) {
+        this.instructorName = instructorName;
+        this.instructorAdress = instructorAdress;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Instructor() {
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+
+    public String getInstructorAdress() {
+        return instructorAdress;
+    }
+
+    public void setInstructorAdress(String instructorAdress) {
+        this.instructorAdress = instructorAdress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<Course> getCourseList() {
+        return courseList;
+    }
+
+    public void setCourseList(List<Course> courseList) {
+        this.courseList = courseList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Instructor that = (Instructor) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "instructorName='" + instructorName + '\'' +
+                ", instructorAdress='" + instructorAdress + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+
 }
